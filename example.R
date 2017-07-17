32ac82ffcbbb6bebc56d86bae003262b7caffeda
Pardo Gomez Carlos Omar <MB75168@cb.bbvabancomer.com.mx>
2017-07-17 14:56:42 -0500

    README's images.

--- example.R
+++ example.R
@@ -1,11 +1,20 @@
+# Installation
+install.packages("devtools")
+library(devtools)
+install_github("opardo/dlmRStan")
+
+# Load package
+library(dlmRStan)
+
+# Load data (Market Research)
 data("dlmRStan3")
 dataset <- dlmRStan3
-
 formula <- awareness ~ .
 
+# Fit model
 model <- dlmRStan(
-  formula = awareness ~ .,
-  dataset = dlmRStan3,
+  formula = formula,
+  dataset = dataset,
   betas_range = c(0, 0.07),
   intercept_range = c(30,50),
   chains = 4,
@@ -13,6 +22,12 @@ model <- dlmRStan(
   warmup = 500
 )
 
+# Fitting validation
 model$validation
 
+# Show insights
 model$insights$plots
+
+# Parameters' errors
+model$parameters$betas_sd
+model$parameters$contribution_sd
